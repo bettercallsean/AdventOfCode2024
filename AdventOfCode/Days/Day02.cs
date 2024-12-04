ba8942adcfb84093f2343d19f2daaf2a972bdd62
Sean Edwards <hello@seanedwards.dev>
2024-12-04 16:26:06 +0000

    Complete rewrite of Day 2 part 1 and 2

--- AdventOfCode/Days/Day02.cs
+++ AdventOfCode/Days/Day02.cs
@@ -15,98 +15,57 @@ public class Day02 : BaseDay
 
         foreach (var report in _input)
         {
-            var mainDirection = 0;
-            for (var i = 0; i < report.Count - 1; i++)
-            {
-                var direction = report[i] - report[i + 1] > 0 ? 1 : -1;
-                
-                if (mainDirection == 0)
-                    mainDirection = direction;
-                
-                if(!SafeLevel(report[i], report[i + 1], direction, mainDirection))
-                    break;
-                
-                if (i != report.Count - 2) continue;
+            var levelDifferences = GetReportDifferences(report);
 
+            if (ReportIsSafe(levelDifferences))
+            {
                 safeReportCount++;
-                break;
             }
         }
-        
+
         return new(safeReportCount.ToString());
     }
 
     public override ValueTask<string> Solve_2()
     {
         var safeReportCount = 0;
+
         foreach (var report in _input)
         {
-            var foo = report
-                // .Select((x, y) => y == 0 ? 0 : x - report[y - 1])
-                // .Select((x, y) => )
-                .Where((x, y) =>  y != 0 && (Math.Abs(x - report[y - 1]) >= 1 || Math.Abs(x - report[y - 1]) <= 3))
-                .ToList();
+            var levelDifferences = GetReportDifferences(report);
 
-            var bar = foo.All(x => x is >= 1 and <= 3 or >= -3 and <= -1);
-            
-            var test = 0;
-            
-            var mainDirection = 0;
-            var problemDampened = false;
-            
-            for (var i = 1; i < report.Count; i++)
+            if (ReportIsSafe(levelDifferences))
             {
-                var direction = report[i] - report[i - 1] > 0 ? 1 : -1;
-                
-                if (mainDirection == 0)
-                    mainDirection = direction;
-
-                if (!SafeLevel(report[i], report[i - 1], direction, mainDirection))
+                safeReportCount++;
+            }
+            else
+            {
+                for (int i = 0; i < report.Count; i++)
                 {
-                    if (problemDampened)
-                        break;
-                
-                    if (i == 1)
-                    {
-                        report.RemoveAt(i);
-                        problemDampened = true;
-                        mainDirection = 0;
-                        i--;
-                        continue;
-                    }
-                    
-                    if (i == report.Count - 1)
+                    var modifiedReport = report.Take(i).Concat(report.Skip(i + 1)).ToList();
+                    levelDifferences = GetReportDifferences(modifiedReport);
+
+                    if (ReportIsSafe(levelDifferences))
                     {
                         safeReportCount++;
                         break;
                     }
-                    
-                    var tempDirection = report[i + 1] - report[i - 1] > 0 ? 1 : -1;
-                    if (SafeLevel(report[i + 1], report[i - 1], tempDirection, mainDirection))
-                    {
-                        report.RemoveAt(i);
-                        i--;
-                        problemDampened = true;
-                    }
-                    else if (SafeLevel(report[i], report[i + 1], tempDirection, tempDirection))
-                    {
-                        problemDampened = true;
-                        mainDirection = tempDirection;
-                    }
-                    else
-                        break;
                 }
-                
-                if (i == report.Count - 1) 
-                    safeReportCount++;
             }
         }
-        
+
         return new(safeReportCount.ToString());
     }
 
-    private static bool SafeLevel(int a, int b, int direction, int mainDirection)
+    private static bool ReportIsSafe(List<int> levelDifferences)
     {
-        return Math.Abs(a - b) <= 3 && Math.Abs(a - b) >= 1 && mainDirection == direction;
+        return levelDifferences.All(x => x is >= 1 and <= 3) || levelDifferences.All(x => x is <= -1 and >= -3);
     }
+
+    private static List<int> GetReportDifferences(List<int> report) => report
+                .Select((x, y) => new { Value = x, Index = y })
+                .Where(x => x.Index != 0 && (Math.Abs(x.Value - report[x.Index - 1]) >= 1 || Math.Abs(x.Value - report[x.Index - 1]) <= 3))
+                .Select(x => x.Value - report[x.Index - 1])
+                .ToList();
 }
+
