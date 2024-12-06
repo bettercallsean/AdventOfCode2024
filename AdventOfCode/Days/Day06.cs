319bafdf46ff8fffcea3c9faa808edcc6f7b713b
Sean Edwards <hello@seanedwards.dev>
2024-12-06 22:27:52 +0000

    Add initial code for day 6 part 2

--- AdventOfCode/Days/Day06.cs
+++ AdventOfCode/Days/Day06.cs
@@ -13,9 +13,11 @@ public class Day06 : BaseDay
         { '<', '^' }
     };
     private readonly (int, int) _startingPosition;
+    private readonly (int, int) _nonExistantBlockade = (int.MaxValue, int.MaxValue);
     private (int, int) _currentPosition;
     private char _direction;
     private char[][] _grid;
+    private Dictionary<char, List<(int, int)>> _knownTurnPoints;
 
     public Day06()
     {
@@ -40,6 +42,13 @@ public class Day06 : BaseDay
         _direction = '^';
         _grid = _input.Select(x => x.ToArray()).ToArray();
         _currentPosition = _startingPosition;
+        _knownTurnPoints = new()
+        {
+            {'^', [] },
+            {'>', [] },
+            {'v', [] },
+            {'<', [] },
+        };
 
         while (validPosition)
         {
@@ -72,6 +81,69 @@ public class Day06 : BaseDay
 
     public override ValueTask<string> Solve_2()
     {
+        var validPosition = true;
+        var potentialBlockageCoordinates = new List<(int, int)>();
+        var count = 0;
+        _grid = _input.Select(x => x.ToArray()).ToArray();
+        _direction = '^';
+        _currentPosition = _startingPosition;
+        _knownTurnPoints = new()
+        {
+            {'^', [] },
+            {'>', [] },
+            {'v', [] },
+            {'<', [] },
+        };
+
+        while (validPosition)
+        {
+            var directionToMove = _direction is '^' or '<' ? -1 : 1;
+
+            switch (_direction)
+            {
+                case '^' or 'v':
+                    {
+                        validPosition = IsValidPosition(directionToMove, 0);
+
+                        var blockadeCoordinates = BlockadeExists(0, _direction is 'v' ? -1 : 1);
+                        if (blockadeCoordinates != _nonExistantBlockade)
+                        {
+                            if (_knownTurnPoints[_positionRotation[_direction]].Contains(blockadeCoordinates))
+                                potentialBlockageCoordinates.Add((_currentPosition.Item1 + directionToMove, _currentPosition.Item2));
+                            else
+                            {
+
+                            }
+                        }
+
+                        //ArrayHelper.ArrayPrinter(_grid);
+
+                        break;
+                    }
+                case '>' or '<':
+                    {
+                        validPosition = IsValidPosition(0, directionToMove);
+
+                        var blockadeCoordinates = BlockadeExists(_direction is '<' ? -1 : 1, 0);
+                        if (blockadeCoordinates != _nonExistantBlockade && _knownTurnPoints[_positionRotation[_direction]].Contains(blockadeCoordinates))
+                        {
+                            potentialBlockageCoordinates.Add((_currentPosition.Item1, _currentPosition.Item2 + directionToMove));
+                        }
+
+                        break;
+                    }
+            }
+
+            if (!validPosition || _grid[_currentPosition.Item1][_currentPosition.Item2] == 'x') continue;
+
+            _grid[_currentPosition.Item1][_currentPosition.Item2] = 'x';
+            count++;
+
+            //Console.Clear();
+            //ArrayHelper.ArrayPrinter(_grid);
+        }
+
+        return new(potentialBlockageCoordinates.Count.ToString());
     }
 
     private bool IsValidPosition(int iDirection, int jDirection)
@@ -83,8 +155,12 @@ public class Day06 : BaseDay
 
             if (_grid[_currentPosition.Item1][_currentPosition.Item2] == '#')
             {
-                _direction = _positionRotation[_direction];
                 _currentPosition = (_currentPosition.Item1 - iDirection, _currentPosition.Item2 - jDirection);
+
+                _knownTurnPoints[_direction].Add(_currentPosition);
+
+                _direction = _positionRotation[_direction];
+
             }
 
             return true;
@@ -93,7 +169,7 @@ public class Day06 : BaseDay
             return false;
     }
 
-    private bool BlockadeExists(int iDirection, int jDirection)
+    private (int, int) BlockadeExists(int iDirection, int jDirection)
     {
         var nextI = _currentPosition.Item1 + iDirection;
         var nextJ = _currentPosition.Item2 + jDirection;
@@ -101,12 +177,13 @@ public class Day06 : BaseDay
         while (ArrayHelper.IsValidCoordinate(nextI, nextJ, _grid))
         {
             if (_grid[nextI][nextJ] == '#')
-                return true;
+                return (nextI - iDirection, nextJ - jDirection);
 
-            nextI = nextI + iDirection;
-            nextJ = nextJ + jDirection;
+            nextI += iDirection;
+            nextJ += jDirection;
         }
 
-        return false;
+        return _nonExistantBlockade;
     }
+
 }
